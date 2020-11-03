06ca0c6a90ba0694ba5c40cf212abf3e6dfde0b2
tsla42 <etsela2@hotmail.com>
2020-11-03 12:45:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+  a=abs(a);
+  b=abs(b);
+  while (a>0 && b>0) {
+    if (a>b) a=a%b; 
+    else b=b%a;
+  }
+  return (a+b) ;
+}
+
+int main() {
+  int N, a1, p1, a2, p2, ar, par;
+  char oper;
+  cout << "arithmos prakseon: " << endl;
+  cin >> N;
+  for (int i=0; i<N; i++) {
+    cin >> oper >> a1 >> p1 >> a2 >> p2 ;
+    if ( (p1==0) || (p2==0) || (oper=='/' && a2==0) )
+      cout << "error" << endl;
+    else {
+     switch (oper) {
+      case '+':
+        ar=a1*p2 + a2*p1;
+        par=p1*p2;
+      break;
+    
+      case '-' :
+        ar=a1*p2 - a2*p1;
+        par=p1*p2;
+      break;
+
+      case '*' :
+        ar=a1*a2;
+        par=p1*p2;
+      break;
+
+      case '/' :
+        ar=a1*p2;
+        par=p1*a2;
+      break;
+      }
+      if ( ((ar>0) && (par<0)) || ((ar<0) && (par>0)) )  
+    cout << "-" << ar/par << " " << (abs(ar%par))/gcd(ar,par) << " " << (abs(par))/gcd(ar,par) << endl;
+    else cout << ar/par << " " << (abs(ar%par))/gcd(ar,par) << " " << (abs(par))/gcd(ar,par) << endl;
+    }
+  }
+  return 0;
+}
